95423be935b5e23148ea60517150d7465eaef9bc
danfaeh <danfaeh@gmail.com>
2016-02-09 21:24:23 -0800

    basic pages plus homepage w/ navigation

--- restful_controller_app/app.rb
+++ restful_controller_app/app.rb
@@ -1,5 +1,22 @@
 class RestfulControllerApp < Sinatra::Base
+  get '/' do
+    @insturments = ["guitar", "saxophone", "bagpipe"]
+    erb :"index"
+  end
 
+  get '/guitar' do
+    @insturments = ["guitar", "saxophone", "bagpipe"]
+    erb :"guitar"
+  end
 
+  get '/saxophone' do
+    @insturments = ["guitar", "saxophone", "bagpipe"]
+    erb :"saxophone"
+  end
+
+  get '/bagpipe' do
+    @insturments = ["guitar", "saxophone", "bagpipe"]
+    erb :"bagpipe"
+  end
 
 end
